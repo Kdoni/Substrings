383693c2f249be699c6f291ee71ed61b27531b00
kdoni <chrisvdoni@gmail.com>
2022-09-22 19:00:58 +0300

    Employing regex. Need to further study the matter.

--- substrings.rb
+++ substrings.rb
@@ -1,16 +1,31 @@
 dictionary = %w[hello people cringe hi yo hell i I goodbye hello good bye blue]
 
-def substrings(word,dictionary)
-    if dictionary.include?(word)
-        result = dictionary.each_with_object(Hash.new(0)) do |item,hash|
-            if item==word
-                hash[word]+=1
-                result
-            end
-        end
-    else
-        "No valid match"
-    end
+def word_modification(word)
+    modified = word.split(/[,_\?\.\!\'\s]+/)
+    p modified
 end
 
-puts substrings("hello",dictionary)
+word_modification("Hello, podia mina_san're?")
+
+# def word_arrayification(pouts)
+#     pouts = [pouts]
+#     pouts
+# end
+
+
+# def substrings(word,dictionary)
+#     word = word_arrayification(word)
+#     p word
+#     if dictionary.include?(word)
+#         result = dictionary.each_with_object(Hash.new(0)) do |item,hash|
+#             if word.include?(item)
+#                 hash[word]+=1
+#                 result
+#             end
+#         end
+#     else
+#         "No valid match"
+#     end
+# end
+
+# puts substrings("hello",dictionary)
