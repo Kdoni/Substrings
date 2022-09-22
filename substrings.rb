d24691aa4fd87b5cfbb8b8baced6596ae1fc3a9a
kdoni <chrisvdoni@gmail.com>
2022-09-22 23:29:20 +0300

    Final touches.

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,32 @@
-dictionary = %w[hello people cringe hi yo hell i I goodbye hello good bye blue]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def word_modification(word)
     modified = word.split(/[,_\?\.\!\'\s]+/)
-    p modified
+    modified
 end
 
-word_modification("Hello, podia mina_san're?")
+def substrings(string,array)
+    final_string = word_modification(string)
+    array.each_with_object(Hash.new(0)) do |value,hash|
+        result = final_string.each do |string_value|
+            if string_value.include?(value)
+                p string_value
+                p value
+                hash[value]+=1
+            else
+                result = "No match found..."
+                break
+            end
+            p result
+        end
+    end 
+end
+
+p substrings("how",dictionary)
+
+
+
+# p word_modification("Hello, podia mina_san're?")
 
 # def word_arrayification(pouts)
 #     pouts = [pouts]
