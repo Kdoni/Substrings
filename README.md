c691f0941bb9da6d4e65615a755bb812a9e71bb1
kdoni <chrisvdoni@gmail.com>
2022-09-23 11:34:25 +0300

    Substrings module.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 An Introductory computer science project.
 Inspired by the Odin Project.
 From the Odin Project curriculum.
+Could further input the regex to split into unnecessary data types.
+Could further improve the logic considering capital and small_case letters.
--- substrings.rb
+++ substrings.rb
@@ -2,51 +2,21 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def word_modification(word)
     modified = word.split(/[,_\?\.\!\'\s]+/)
-    modified
+    modified.map{|word| word.downcase}
 end
 
 def substrings(string,array)
     final_string = word_modification(string)
-    array.each_with_object(Hash.new(0)) do |value,hash|
-        result = final_string.each do |string_value|
+    result = array.each_with_object(Hash.new(0)) do |value,hash|
+        final_string.each do |string_value|
             if string_value.include?(value)
-                p string_value
-                p value
                 hash[value]+=1
-            else
-                result = "No match found..."
-                break
             end
-            p result
         end
-    end 
+    end
+    if result.empty?
+        "No valid matches..."
+    else
+        result
+    end
 end
-
-p substrings("how",dictionary)
-
-
-
-# p word_modification("Hello, podia mina_san're?")
-
-# def word_arrayification(pouts)
-#     pouts = [pouts]
-#     pouts
-# end
-
-
-# def substrings(word,dictionary)
-#     word = word_arrayification(word)
-#     p word
-#     if dictionary.include?(word)
-#         result = dictionary.each_with_object(Hash.new(0)) do |item,hash|
-#             if word.include?(item)
-#                 hash[word]+=1
-#                 result
-#             end
-#         end
-#     else
-#         "No valid match"
-#     end
-# end
-
-# puts substrings("hello",dictionary)
